a0e10c1a04490016637f2f8bd277792c0ab6380a
Pablo Blazquez <pbl@foryouandyourcustomers.com>
2019-01-15 08:32:06 +0100

    refactoring to multimodel maven project. close #1

---
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package sfgpetclinic;
+
+import org.junit.Test;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class SfgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
